d5a7d2759c2c9a6b7a86ebd829c40233df31d0ef
today-is-cinthia <87044276+today-is-cinthia@users.noreply.github.com>
2022-04-03 22:03:34 -0600

    Add files via upload

---
+++ application.py
@@ -0,0 +1,66 @@
+
+#from crypt import methods
+import os
+from turtle import title
+
+from flask import Flask, session, flash, redirect, render_template, request, session, url_for, jsonify
+from flask_session import Session
+from sqlalchemy import create_engine
+from sqlalchemy.orm import scoped_session, sessionmaker
+from dotenv import load_dotenv
+from werkzeug.security import check_password_hash, generate_password_hash
+
+from loginrequired import login_required
+
+app = Flask(__name__)
+
+#Check for environment variable
+if not os.getenv("DATABASE_URL"):
+    raise RuntimeError("DATABASE_URL is not set")
+
+# Configure session to use filesystem
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
+
+# Set up database
+engine = create_engine(os.getenv("DATABASE_URL"))
+db = scoped_session(sessionmaker(bind=engine))
+
+
+@app.route("/")
+def index():
+    return render_template("inicio.html")
+
+
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    if request.method == "POST":
+        user = request.form.get("username")
+        password = request.form.get("password")
+        confirmation = request.form.get("confirmation")
+
+        if confirmation != password:
+            flash("passwords dont match")
+            return render_template("register.html")
+
+        rows = db.execute(
+            "SELECT usuario FROM users WHERE usuario=:username", {"username": user}).fetchall()
+
+        if not rows:
+            db.execute("INSERT INTO users (usuario, contraseña) VALUES (:username, :password)",
+                       {"username": user, "password": generate_password_hash(password)})
+            db.commit()
+        elif rows:
+            flash("username already exists")
+            return render_template("register.html")
+
+        return redirect("/registros")
+
+    else:
+        return render_template("register.html")
+
+@app.route("/registros")
+def registros():
+    return render_template("registros.html")
+
